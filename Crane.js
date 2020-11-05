390804089640fea7608956ee4aa5ead56ed0358d
Kunaal-l <71383275+Kunaal-l@users.noreply.github.com>
2020-11-05 17:20:11 +0530

    Add files via upload

---
+++ Crane.js
@@ -0,0 +1,38 @@
+class Crane{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 2,
+            length: 300
+        }
+        this.crane = loadImage('crane.png');
+        
+        this.pointB = pointB
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+   
+    
+    
+    display(){
+        //image(this.crane,200,20);
+       
+        if(this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
+        
+            
+            stroke(48,22,8);
+            
+         line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+             
+                
+         
+           
+            
+        
+        }
+    }
+    
+}
